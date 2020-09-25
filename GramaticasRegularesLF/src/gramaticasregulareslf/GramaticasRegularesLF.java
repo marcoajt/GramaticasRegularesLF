419ddc25401d70000ddf44d330818e4de17d7ed3
marcoajt <marcoantoniojt@gmail.com>
2020-09-25 15:23:22 -0300

    Inicio do projeto

--- GramaticasRegularesLF/src/gramaticasregulareslf/GramaticasRegularesLF.java
+++ GramaticasRegularesLF/src/gramaticasregulareslf/GramaticasRegularesLF.java
@@ -1,21 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gramaticasregulareslf;
 
-/**
- *
- * @author marco
- */
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class GramaticasRegularesLF {
 
-    /**
-     * @param args the command line arguments
-     */
+    Raizes S;
+    Terminais E;
+
     public static void main(String[] args) {
         // TODO code application logic here
+
+        Scanner argumento = null;
+        String teste;
+        System.out.println("Informe as raizes:");
+        teste = argumento.next();
+        System.out.println(teste);
+        /* while(argumento.hasNext())  
+        {
+
+        }*/
+        System.out.println("Informe os terminais:");
+
     }
-    
+
 }
