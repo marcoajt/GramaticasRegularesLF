0f2ac4facaafc88b90cf089108190efd43130c16
dias_ <dias_@LAPTOP-9BQH6VBF>
2020-10-04 02:39:01 -0300

    Nova versão, muitas novidades

--- GramaticasRegularesLF/src/gramaticasregulareslf/Gramatica_Regular.java
+++ GramaticasRegularesLF/src/gramaticasregulareslf/Gramatica_Regular.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public class Gramatica_Regular {
 
-    public List<String> raizes;
+    public List<String> nao_terminais;
     public List<String> terminais;
     public List<Regras> conj_regras;
 
     public Gramatica_Regular() {
-        this.raizes = new ArrayList();
+        this.nao_terminais = new ArrayList();
         this.terminais = new ArrayList();
         this.conj_regras = new ArrayList();//Uma lista de listas
 
--- GramaticasRegularesLF/src/gramaticasregulareslf/GramaticasRegularesLF.java
+++ GramaticasRegularesLF/src/gramaticasregulareslf/GramaticasRegularesLF.java
@@ -4,57 +4,115 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GramaticasRegularesLF {
-//criação do objeto
 
     static Gramatica_Regular G_R = new Gramatica_Regular();
-    //private static String argumento = "";
-    //private static final Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         String argumento;
         Scanner scan = new Scanner(System.in);
+        String alfa = new String();
+        char cont_alfa[];
+        boolean verificador;
 
-        System.out.println("Informe as raizes " + "\n(adicione cada uma com enter ao fim)" + "\n digite @ para avancar: ");
+        alfa = alfa + "&";
+
+        System.out.println("Informe as variaveis nao-terminais \n (adicione cada uma com enter ao fim) \n digite @ para avancar: ");
         argumento = scan.next();
         while (!"@".equals(argumento)) {
-            G_R.raizes.add(argumento);
-            G_R.conj_regras.add(new Regras());
+            verificador = false;
+            for (int x = 0; x < G_R.nao_terminais.size(); x++) {
+                if (G_R.nao_terminais.get(x).contains(argumento)) {
+                    System.out.println("\nSimbolo invalido! Ja existente nas variaveis nao-terminais\n");
+                    verificador = true;
+                }
+            }
+            if (argumento.contains("&")) {
+                System.out.println("\nSimbolo invalido! Ja existente simbolizando o vazio\n");
+            }
+            if (verificador == false) {
+                G_R.nao_terminais.add(argumento);
+                G_R.conj_regras.add(new Regras());
+                alfa = alfa + argumento;
+                System.out.println("\nInserido com sucesso!\n");
+            }
             argumento = scan.next();
         }
 
-        System.out.println("Informe as terminais" + "\n(adicione cada uma com enter ao fim)" + "\n digite @ para avancar: ");
+        System.out.println("Informe as variaveis terminais \n(adicione cada uma com enter ao fim) \n digite @ para avancar: ");
         argumento = scan.next();
         while (!"@".equals(argumento)) {
-            G_R.terminais.add(argumento);
+            verificador = false;
+            for (int x = 0; x < G_R.nao_terminais.size(); x++) {
+                if (G_R.nao_terminais.get(x).contains(argumento)) {
+                    System.out.println("\nSimbolo invalido! Ja existente nas variaveis nao-terminais\n");
+                    verificador = true;
+                }
+            }
+            for (int x = 0; x < G_R.terminais.size(); x++) {
+                if (G_R.terminais.get(x).contains(argumento)) {
+                    System.out.println("\nSimbolo invalido! Ja existente nas variaveis terminais\n");
+                    verificador = true;
+                }
+            }
+            if (argumento.contains("&")) {
+                System.out.println("\nSimbolo invalido! Ja existente simbolizando o vazio\n");
+                verificador = true;
+            }
+            if (verificador == false) {
+                G_R.terminais.add(argumento);
+                alfa = alfa + argumento;
+                System.out.println("\nInserido com sucesso!\n");
+            }
+
             argumento = scan.next();
         }
 
         int i = 0;
         do {
             //adição da funçção de pra testa se o parametro já existe em terminais
-            System.out.println("Informe as regras para a raiz |" + G_R.raizes.get(i) + "|" + "\n(adicione cada uma com enter ao fim)" + "\ndigite @ para avancar: ");
+            System.out.println("Informe as regras para a variavel nao-terminal |" + G_R.nao_terminais.get(i) + "| \n A gramatica regular deve ser linear a esquerda ou a direita\nPara usar o vazio utilize o simbolo '&' \n (adicione cada uma com enter ao fim) \n digite @ para avancar: ");
             argumento = scan.next();
             while (!"@".equals(argumento)) {
                 G_R.conj_regras.get(i).derivacoes.add(argumento);
+                System.out.println("\nInserido com sucesso!\n");
                 argumento = scan.next();
             }
             i++;
         } while (i < G_R.conj_regras.size());
-        /*
-         //EXIBIÇÃO
+        cont_alfa = alfa.toCharArray();
+
+        ///*
+        //EXIBIÇÃO
+        System.out.println("Regras:");
         for (int x = 0; x < G_R.conj_regras.size(); x++) {
             int z = G_R.conj_regras.get(x).derivacoes.size();
             for (int y = 0; y < z; y++) {
                 System.out.print(G_R.conj_regras.get(x).derivacoes.get(y) + " ");
             }
-            System.out.println("");
-        }*/
+            System.out.println("\n");
+        }
+        System.out.println("Alfabeto:");
+        for (int x = 0; x < cont_alfa.length; x++) {
+            System.out.print(cont_alfa[x] + " - ");
+        }
+        System.out.println("\n");
+        //*/
 
-        System.out.println("Informe um termo para ser derivado" + "\n(adicione cada uma com enter ao fim)" + "\n digite @ para finalizar: ");
+        System.out.println("Informe o simbolo inicial (raiz) para gerar aleatoriamente uma palavra pertencente a esta gramatica regular \n (adicione com enter ao fim) \n digite @ para finalizar: ");
         argumento = scan.next();
         while (!"@".equals(argumento)) {
-            derivador(argumento);
+
+            verificador = false;
+            for (int x = 0; x < G_R.nao_terminais.size(); x++) {
+                if (G_R.nao_terminais.get(x).contains(argumento) && !argumento.contains("@")) {
+                    derivador(argumento);
+                    verificador = true;
+                }
+            }
+            if (verificador == false) {
+                System.out.println("\nSimbolo invalido!\n");
+            }
             argumento = scan.next();
         }
 
@@ -63,33 +121,46 @@ public class GramaticasRegularesLF {
     public static void derivador(String parametro) {
         String resultado = new String();
         boolean aux = false;
-        Random er = new Random();
+        Random choice = new Random();
         char origem[] = parametro.toCharArray();
+        char finale[];
 
-        for (int x = 0; x < G_R.raizes.size(); x++) {
+        for (int x = 0; x < G_R.nao_terminais.size(); x++) {
             for (int y = 0; y < origem.length; y++) {
 
                 StringBuilder aux1 = new StringBuilder();
                 aux1.append(origem[y]);
                 String aux2 = aux1.toString();
-                if (G_R.raizes.get(x).equals(aux2)) {
+
+                if (G_R.nao_terminais.get(x).equals(aux2)) {
                     aux = true;
                     for (int z = 0; z < origem.length; z++) {
-                        if (y != z) {
+                        if (y != z && origem[z] != '&') {
                             StringBuilder argamassa = new StringBuilder();
                             argamassa.append(origem[z]);
                             resultado = resultado + argamassa.toString();
-                        } else {
-
-                            resultado = resultado + G_R.conj_regras.get(x).derivacoes.get(er.nextInt(G_R.conj_regras.get(x).derivacoes.size()));
+                        }
+                        if (y == z) {
+                            resultado = resultado + G_R.conj_regras.get(x).derivacoes.get(choice.nextInt(G_R.conj_regras.get(x).derivacoes.size()));
                         }
                     }
-
                 }
+
             }
         }
-        System.out.println("|" + resultado + "|");
+
+        finale = resultado.toCharArray();
+        resultado = "";
+        for (int z = 0; z < finale.length; z++) {
+            if (finale[z] != '&') {
+                StringBuilder argamassa = new StringBuilder();
+                argamassa.append(finale[z]);
+                resultado = resultado + argamassa.toString();
+            }
+        }
+
         if (aux == true) {
+            System.out.println("-> " + resultado);
             derivador(resultado);
         }
     }
